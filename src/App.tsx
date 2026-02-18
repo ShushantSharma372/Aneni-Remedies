05a00124e575c183c26ee3373f9af2c6f4a986b7
Shushant Sharma <91726756+ShushantSharma372@users.noreply.github.com>
2026-02-18 13:45:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import WhatsAppFloat from "./components/WhatsAppFloat";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
@@ -23,6 +24,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <WhatsAppFloat />
       <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
